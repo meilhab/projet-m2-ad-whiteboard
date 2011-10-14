29af365fc0050f9bd3bfc30b35df0a7fc673e33c
benoit.meilhac@gmail.com <benoit.meilhac@gmail.com@147f6cb1-3408-4ae9-7e5b-ef82d0c5dd32>
2011-10-14 15:01:06 +0000

    modification : test2 devient LanceurLamport -> pour la forme

--- m2-ad-projet/src/lanceur/LanceurLamport.java
+++ m2-ad-projet/src/lanceur/LanceurLamport.java
@@ -5,60 +5,102 @@ import java.rmi.NotBoundException;
 import java.rmi.RemoteException;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
+import java.util.Random;
 
 import protocoles.ILamport;
 import protocoles.Lamport;
 
-/**
- * coucou
- * @author cmichou2
- *
- */
 public class LanceurLamport {
-	public static void main(String[] args) throws InterruptedException, IOException {
-		Lamport myProc = new Lamport(0);
-		ILamport neighbors[] = new ILamport[5];
 
-		Registry registry = null;
+	static int procNumber;
+	static int nbProcesses;
+	static Lamport myProc;
+	static ILamport[] neighbors; 
+	
+	public static final int registryPort = 5555;
+	
+	public static void main( String args[]) throws InterruptedException, IOException {
+		
+		procNumber =  Integer.parseInt(args[0]);
+		nbProcesses =  Integer.parseInt(args[1]);
+
+	
+		neighbors = new ILamport[nbProcesses];
+		
 		try {
-			// Declare Bully Object to registry
-			registry = LocateRegistry.createRegistry(5555);
-		} catch (java.rmi.server.ExportException ee) {
-			registry = LocateRegistry.getRegistry(5555);
-		}
-
-		registry.rebind("Test Lamport", myProc);
-
-		// wait for all neighbors to be recorded
-		try {
-			Thread.sleep(20000);
-		} catch (InterruptedException e) {
+			myProc = new Lamport( procNumber );
+	
+			Registry registry = null;
+			try{
+				// Declare Lamport Object to registry
+				registry = LocateRegistry.	createRegistry(registryPort);
+			} catch ( java.rmi.server.ExportException ee ) {
+				registry = LocateRegistry.getRegistry(registryPort);
+			}
+			
+			//Create Name en record in registry
+			String Name = new String("Lamport"+procNumber);
+			System.out.println("Declare " + Name);
+			registry.rebind( Name , myProc );
+			
+			// wait for all neighbors to be recorded
+			try {
+				Thread.sleep(10000);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+			
+			// Get list of neighbors names
+			String[] neighborsNames = registry.list();
+			System.out.println("Participants a l'election");
+			for ( String name : neighborsNames ) {
+				System.out.println(" Nom : " + name);
+			}
+			for( int i=0 ; i < neighborsNames.length ; i++) {
+				try {
+					neighbors[i] = (ILamport) registry.lookup( neighborsNames[i] );
+					neighbors[i].test( procNumber);
+				} catch (NotBoundException e) {
+					e.printStackTrace();
+				}
+			}
+			myProc.setVoisins( neighbors );
+		} catch (RemoteException e) {	
 			e.printStackTrace();
 		}
 
-		String[] neighborsNames = registry.list();
-		System.out.println("Participants a l'election");
-		for (String name : neighborsNames) {
-			System.out.println(" Nom : " + name);
-		}
 		
-		for (int i = 0; i < neighborsNames.length; i++) {
-			try {
-				neighbors[i] = (ILamport) registry.lookup(neighborsNames[i]);
-			} catch (NotBoundException e) {
-				e.printStackTrace();
-			}
-		}
-		
-		myProc.setVoisins( neighbors );
-
+		// wait for all neighbors to be recorded and ready
 		try {
 			Thread.sleep(10000);
 		} catch (InterruptedException e) {
 			e.printStackTrace();
 		}
-
-		myProc.demandeAcces();
-
+		
+		if ( procNumber == 0 ) {
+			myProc.demandeAcces();
+		} else {
+			System.out.println("Waiting for 10000 ms");
+			try {
+				Thread.sleep(10000);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+		
+		// Wait for election and randomly start a new one
+		while(true) {
+				Random r = new Random(System.currentTimeMillis());
+				int waitingTime = (r.nextInt(20)+10)*1000;
+				System.out.println("Waiting for " + waitingTime + " ms");
+				try {
+					Thread.sleep(waitingTime);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+				
+		}	
 	}
+
+	
 }
