5b33f9cd7a2bd7c9e35364198ed0326cc4687bbe
benoit.meilhac@gmail.com <benoit.meilhac@gmail.com@147f6cb1-3408-4ae9-7e5b-ef82d0c5dd32>
2011-10-14 11:11:22 +0000

    ajout d'un autre lanceur test reprenant Bully -déplacement dans le /bin du projet -rmiregistry -lancement de 5 terminaux : java lanceur.test2 i 5 ->revoir la condition pour passer en section critique : même en ayant reçu tous les ACK, elle ne se valide pas. Erreur dans la condition ?, ou dans les fonctions de message : erreur d'indice ?

--- trunk/m2-ad-projet/src/protocoles/ILamport.java
+++ trunk/m2-ad-projet/src/protocoles/ILamport.java
@@ -10,5 +10,7 @@ public interface ILamport extends Remote {
 	public void recoitAck(int horloge, int idClient) throws RemoteException;
 
 	public void recoitRel(int horloge, int idClient) throws RemoteException;
+	
+	public void test(int idClient) throws RemoteException;
 
 }
