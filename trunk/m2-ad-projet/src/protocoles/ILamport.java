197bee8c3c53388da71d9461307f302994fb9733
benoit.meilhac@gmail.com <benoit.meilhac@gmail.com@147f6cb1-3408-4ae9-7e5b-ef82d0c5dd32>
2011-10-14 12:51:09 +0000

    problème condition résolu intégration d'un gestionnaire de log afin de récupérer les traces

--- trunk/m2-ad-projet/src/protocoles/ILamport.java
+++ trunk/m2-ad-projet/src/protocoles/ILamport.java
@@ -1,15 +1,16 @@
 package protocoles;
 
+import java.io.IOException;
 import java.rmi.Remote;
 import java.rmi.RemoteException;
 
 public interface ILamport extends Remote {
 
-	public void recoitReq(int horloge, int idClient) throws RemoteException;
+	public void recoitReq(int horloge, int idClient) throws IOException;
 
-	public void recoitAck(int horloge, int idClient) throws RemoteException;
+	public void recoitAck(int horloge, int idClient) throws IOException;
 
-	public void recoitRel(int horloge, int idClient) throws RemoteException;
+	public void recoitRel(int horloge, int idClient) throws IOException;
 	
 	public void test(int idClient) throws RemoteException;
 
--- trunk/m2-ad-projet/src/protocoles/Lamport.java
+++ trunk/m2-ad-projet/src/protocoles/Lamport.java
@@ -1,6 +1,9 @@
 package protocoles;
 
+import java.io.IOException;
 import java.rmi.RemoteException;
+
+import log.LogManager;
 
 public class Lamport extends Protocole implements ILamport, Runnable {
 	private static final long serialVersionUID = 995286733645484409L;
@@ -13,6 +16,7 @@ public class Lamport extends Protocole implements ILamport, Runnable {
 	private int t_horloge[];
 	private TypeMessage t_message[];
 	private ILamport voisins[];
+	private LogManager log;
 
 	public Lamport(int idClient) throws RemoteException {
 		super();
@@ -23,32 +27,39 @@ public class Lamport extends Protocole implements ILamport, Runnable {
 			t_horloge[i] = 0;
 		}
 		this.idClient = idClient;
+		log = new LogManager(LogManager.PROTOCOLE);
 	}
 
 	public void setVoisins(ILamport voisins[]) {
 		this.voisins = voisins;
 	}
 
-	public void demandeAcces() throws RemoteException, InterruptedException {
-		System.out.println("[" + idClient + "]" + "demande l'accès en section critique");
+	public void demandeAcces() throws InterruptedException, IOException {
+		log.log("[" + idClient + "]" + "demande l'accès en section critique");
+
 		horloge++;
-		for (int i = 0; i < t_horloge.length; i++) {
-			if (i != idClient) {
-				voisins[i].recoitReq(horloge, idClient);
-			}
-		}
 		t_horloge[idClient] = horloge;
 		t_message[idClient] = TypeMessage.REQ;
 
+		for (int i = 0; i < t_horloge.length; i++) {
+			if (i != idClient) {
+				log.log("[" + idClient + "]envoi REQ(" + horloge + ") à " + i);
+
+				voisins[i].recoitReq(horloge, idClient);
+			}
+		}
+
 		synchronized (this) {
-			System.out.println("[" + idClient + "]" + "attend la confirmation des autres clients");
+			log.log("[" + idClient + "]"
+					+ "attend la confirmation des autres clients");
 			boolean finAttente = false;
 			while (!finAttente) {
 				finAttente = true;
-				//TODO : correction de la condition à faire ici, elle ne passe pas même en ayant reçu un ACK de tous les autres
+
 				for (int i = 0; i < voisins.length; i++) {
 					if (i != idClient) {
 						if (!((t_horloge[idClient] < t_horloge[i]) || ((t_horloge[idClient] == t_horloge[i]) && (idClient < i)))) {
+							log.log("\t -> confirmation de" + "[" + i + "]");
 							finAttente = false;
 						}
 					}
@@ -62,24 +73,26 @@ public class Lamport extends Protocole implements ILamport, Runnable {
 		/**
 		 * Entrée en section critique
 		 */
-		System.out.println("[" + idClient + "]" + "entre en section critique");
+		log.log("[" + idClient + "]entre en section critique");
 	}
 
 	public synchronized void recoitReq(int horloge, int idClient)
-			throws RemoteException {
-		System.out.println("[" + this.idClient + "]"
-				+ "recoit le message REQ de" + "[" + idClient + "]");
+			throws IOException {
+		log.log("[" + this.idClient + "]recoit REQ de" + "[" + idClient + "]");
+
 		this.horloge = max(this.horloge, horloge) + 1;
 		t_horloge[idClient] = horloge;
 		t_message[idClient] = TypeMessage.REQ;
+
+		log.log("[" + this.idClient + "]envoi ACK à[" + idClient + "]");
 		voisins[idClient].recoitAck(this.horloge, this.idClient);
 		this.notify();
 	}
 
 	public synchronized void recoitAck(int horloge, int idClient)
-			throws RemoteException {
-		System.out.println("[" + this.idClient + "]"
-				+ "recoit le message ACK de" + "[" + idClient + "]");
+			throws IOException {
+		log.log("[" + this.idClient + "]recoit ACK de" + "[" + idClient + "]");
+
 		this.horloge = max(this.horloge, horloge) + 1;
 		if (t_message[idClient] != TypeMessage.REQ) {
 			t_horloge[idClient] = horloge;
@@ -88,11 +101,13 @@ public class Lamport extends Protocole implements ILamport, Runnable {
 		}
 	}
 
-	public void libereAcces() throws RemoteException {
-		System.out.println("[" + idClient + "]" + "sort de section critique");
+	public void libereAcces() throws IOException {
+		log.log("[" + idClient + "]sort de section critique");
+
 		horloge = horloge + 1;
 		for (int i = 0; i < voisins.length; i++) {
 			if (i != idClient) {
+				log.log("[" + idClient + "]envoi REL(" + horloge + ") à " + i);
 				voisins[i].recoitRel(horloge, idClient);
 			}
 		}
@@ -102,16 +117,16 @@ public class Lamport extends Protocole implements ILamport, Runnable {
 	}
 
 	public synchronized void recoitRel(int horloge, int idClient)
-			throws RemoteException {
-		System.out.println("[" + this.idClient + "]"
-				+ "recoit le message REL de" + "[" + idClient + "]");
+			throws IOException {
+		log.log("[" + this.idClient + "]recoit REL de" + "[" + idClient + "]");
+
 		this.horloge = max(this.horloge, horloge) + 1;
 		t_horloge[idClient] = horloge;
 		t_message[idClient] = TypeMessage.REL;
 		this.notify();
 	}
-	
-	public void test(int idClient) throws RemoteException{
+
+	public void test(int idClient) throws RemoteException {
 		System.out.println("test ->" + this.idClient + " de ->" + idClient);
 	}
 
@@ -129,6 +144,9 @@ public class Lamport extends Protocole implements ILamport, Runnable {
 		} catch (RemoteException e) {
 			e.printStackTrace();
 		} catch (InterruptedException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
