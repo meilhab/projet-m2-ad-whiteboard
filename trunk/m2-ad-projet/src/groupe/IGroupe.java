f712bf2c27325bb9bd85bcb9e50fe994e557c679
benoit.meilhac@gmail.com <benoit.meilhac@gmail.com@147f6cb1-3408-4ae9-7e5b-ef82d0c5dd32>
2011-10-18 14:20:25 +0000

    début groupe + changement lamport

--- trunk/m2-ad-projet/src/groupe/IGroupe.java
+++ trunk/m2-ad-projet/src/groupe/IGroupe.java
@@ -1,5 +1,14 @@
 package groupe;
 
-public interface IGroupe {
+import java.io.IOException;
+import java.rmi.Remote;
 
+import protocoles.ILamport;
+
+public interface IGroupe extends Remote{
+
+	public void enregistrementClient(ILamport il);
+	
+	public void receptionMessage(int tm, int idEnvoi, int idDestination,
+			int horloge) throws IOException, InterruptedException;
 }
