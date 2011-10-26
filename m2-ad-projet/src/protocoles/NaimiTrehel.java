5f977664e01f7dc64565703f266c6afaa38e9727
colin.michoudet <colin.michoudet@147f6cb1-3408-4ae9-7e5b-ef82d0c5dd32>
2011-10-26 15:58:46 +0000

    RC de Naomi Campbell ! Semble marché mais le fait que le jeton puisse être envoyer à deux endroits différents peut peut-être posser problème.

--- m2-ad-projet/src/protocoles/NaimiTrehel.java
+++ m2-ad-projet/src/protocoles/NaimiTrehel.java
@@ -9,13 +9,41 @@ import log.LogManager;
 public class NaimiTrehel extends Protocole implements INaimiTrehel, IProtocole {
 
 	private static final long serialVersionUID = -7741580523855159132L;
+	/**
+	 * Identifiant du site supposé posseder le jeton 
+	 */
 	public int owner;
+	
+	/**
+	 * Identifiant du site a qu envoyer le jeton, initialisé a -1 (nil)
+	 */
 	public int next;
+	
+	/**
+	 * Boolean indiquant si le site possede le jeton, initialise a faux 
+	 */
 	public boolean tocken;
+	
+	/**
+	 * Boolean indiquant si le site a demande la SC, initialise a faux
+	 */
 	public boolean requesting;
+	
+	/**
+	 * Tableau des voisins
+	 */
 	private INaimiTrehel voisins[];
+	
+	/**
+	 * Le log pour les messages
+	 */
 	private LogManager log;
 
+	/**
+	 * 
+	 * @param idClient
+	 * @throws RemoteException
+	 */
 	public NaimiTrehel(int idClient) throws RemoteException {
 		super();
 		this.idClient = idClient;
@@ -29,10 +57,17 @@ public class NaimiTrehel extends Protocole implements INaimiTrehel, IProtocole {
 
 	}
 
+	/**
+	 * 
+	 * @param voisins
+	 */
 	public void setVoisins(INaimiTrehel[] voisins) {
 		this.voisins = voisins;
 	}
 
+	/**
+	 * Donne le jeton au proc elu, indique le owner aux autres.
+	 */
 	public void initialisation(int electednode) throws RemoteException {
 		if (idClient == electednode) {
 			tocken = true;
@@ -42,6 +77,11 @@ public class NaimiTrehel extends Protocole implements INaimiTrehel, IProtocole {
 		}
 	}
 
+	/**
+	 * 
+	 * @throws IOException
+	 * @throws InterruptedException
+	 */
 	public void demandeAcces() throws IOException, InterruptedException {
 		log.log("[" + idClient + "]" + "demande l'accès en section critique");
 		requesting = true;
@@ -57,12 +97,19 @@ public class NaimiTrehel extends Protocole implements INaimiTrehel, IProtocole {
 					this.wait();
 				}
 			}
-		} else {
-			sectionCritique();
+			log.log("[" + idClient + "]Jeton recu");
 		}
+		sectionCritique();
+
 	}
 
-	private synchronized void sectionCritique() throws IOException, InterruptedException {
+	/**
+	 * 
+	 * @throws IOException
+	 * @throws InterruptedException
+	 */
+	private synchronized void sectionCritique() throws IOException,
+			InterruptedException {
 		log.log("[" + this.idClient + "]entre en section critique");
 		Thread.sleep(5000);
 		/**
@@ -78,18 +125,19 @@ public class NaimiTrehel extends Protocole implements INaimiTrehel, IProtocole {
 	@Override
 	public synchronized void recoitReq(int idRequester, int idSender)
 			throws IOException, RemoteException {
-		log.log("[" + idClient + "]Recoit REQ(" + idRequester + ") de " + idSender);
+		log.log("[" + idClient + "]Recoit REQ(" + idRequester + ") de "
+				+ idSender);
 		if (owner == -1) {
 			if (requesting) {
 				next = idRequester;
 			} else {
 				tocken = false;
-				log.log("[" + idClient + "]envoi JETON() à[" + idRequester
-						+ "]");
+				log.log("[" + idClient + "]envoi JETON() à " + idRequester);
 				voisins[idRequester].recoitJeton();
 				owner = idRequester;
 			}
 		} else {
+			log.log("[" + idClient + "]Fait suivre REQ("+ idRequester +") a " + owner);
 			voisins[owner].recoitReq(idRequester, idClient);
 		}
 	}
@@ -100,9 +148,15 @@ public class NaimiTrehel extends Protocole implements INaimiTrehel, IProtocole {
 		this.notify();
 	}
 
-	public void libereAcces() throws RemoteException {
+	/**
+	 * 
+	 * @throws IOException
+	 */
+	public synchronized void libereAcces() throws IOException {
+		log.log("[" + idClient + "]Sortie de SC");
 		requesting = false;
 		if (next != -1) {
+			log.log("[" + idClient + "]envoi JETON() a " + next);
 			voisins[next].recoitJeton();
 			tocken = false;
 			next = -1;
